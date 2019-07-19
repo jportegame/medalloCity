a87da081c0dd6e1db9e075d5ebf9b6c66e426fac
Juanes Cendales <juanescendales@gmail.com>
2019-07-18 20:08:29 -0500

    Grafos 2

--- src/inmovil/GrafoVias.scala
+++ src/inmovil/GrafoVias.scala
@@ -1,21 +1,21 @@
 package inmovil
 import scalax.collection.Graph
 import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
-import scalax.collection.edge.WDiEdge
+import scalax.collection.edge.WLDiEdge
 import ciudad.Interseccion
 import ciudad.Via
 import ciudad.Sentido
 import scala.collection.mutable.ArrayBuffer
 
 object GrafoVias {
-  var g = Graph[Interseccion, WDiEdge]()
+  var g = Graph[Interseccion, WLDiEdge]()
   def construir(vias: ArrayBuffer[Via]):Unit = {
     vias.foreach(x=> {
       if((x.sentido.nombre).equalsIgnoreCase("Un Sentido")){
-        g = (g + WDiEdge(x.origen, x.fin)(x.distancia))
+        g = (g + WLDiEdge(x.origen, x.fin)(x.distancia,x))
       }else{
-        g = (g + WDiEdge(x.origen, x.fin)(x.distancia))
-        g = (g + WDiEdge(x.fin, x.origen)(x.distancia))
+        g = (g + WLDiEdge(x.origen, x.fin)(x.distancia,x))
+        g = (g + WLDiEdge(x.fin, x.origen)(x.distancia,x))
       }
     })
     
