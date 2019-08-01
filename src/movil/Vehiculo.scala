098d44ffad2a8c42359dfe270027c751316bd489
Juanma <jpajoyl@unal.edu.co>
2019-08-01 15:49:13 -0500

    proporciones

--- src/movil/Vehiculo.scala
+++ src/movil/Vehiculo.scala
@@ -2,28 +2,24 @@ package movil
 import inmovil._
 import scala.collection.mutable.Set
 
-case class Vehiculo(var placa:String = "")(pos: Punto, vel: Velocidad) extends Movil(pos, vel) with MovimientoUniforme {
+case class Vehiculo(var placa: String = "")(pos: Punto, vel: Velocidad) extends Movil(pos, vel) with MovimientoUniforme {
 
   private var _detenido = false
 
   def detenido = _detenido
   def detenido_=(detenido: Boolean) = _detenido = detenido
-  
-
-
 
 }
 
 object Vehiculo {
-  
-  val setPlacas:Set[String] = Set()
-  
-  
-  val pCarros:Double = Simulacion.parametros.pametrosSimulacion.proporciones.carros
-  val pMotos:Double = Simulacion.parametros.pametrosSimulacion.proporciones.motos + pCarros
-  val pBuses:Double = Simulacion.parametros.pametrosSimulacion.proporciones.buses + pMotos
-  val pCamiones:Double = Simulacion.parametros.pametrosSimulacion.proporciones.camiones + pBuses
-  val pMotoTaxis:Double = Simulacion.parametros.pametrosSimulacion.proporciones.motoTaxis + pCamiones
+
+  val setPlacas: Set[String] = Set()
+
+  val pCarros: Double = 0.4 //Simulacion.parametros.pametrosSimulacion.proporciones.carros
+  val pMotos: Double = 0.3 + pCarros //Simulacion.parametros.pametrosSimulacion.proporciones.motos + pCarros
+  val pBuses: Double = 0.15 + pMotos //Simulacion.parametros.pametrosSimulacion.proporciones.buses + pMotos
+  val pCamiones: Double = 0.1 + pBuses //Simulacion.parametros.pametrosSimulacion.proporciones.camiones + pBuses
+  val pMotoTaxis: Double = 0.05 + pCamiones //Simulacion.parametros.pametrosSimulacion.proporciones.motoTaxis + pCamiones
 
   def apply(pos: Punto, vel: Velocidad): Vehiculo = {
     val r = new scala.util.Random
@@ -46,6 +42,5 @@ object Vehiculo {
     }
 
   }
-
 
 }
